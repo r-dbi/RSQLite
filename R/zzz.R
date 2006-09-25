35f9de8dd3904387241d27ee42bd01a8f074e28e
sethf <sethf@dcde13d4-9b1b-0410-ac9e-ef07de68c835>
2006-09-25 19:07:47 +0000

    Cleanup .First.lib
    Removed unneeded calls to library.
    
    
    git-svn-id: https://hedgehog.fhcrc.org/compbio/r-dbi/trunk@188 dcde13d4-9b1b-0410-ac9e-ef07de68c835

--- R/zzz.R
+++ R/zzz.R
@@ -1,23 +1,13 @@
 ## $Id$
-".conflicts.OK" <- TRUE
-
-## these are needed while source'ing the package (prior
-## to library.dyname).
-library(methods, warn.conflicts = FALSE)
-library(DBI, warn.conflicts = FALSE)
-
 ".First.lib" <- 
-function(lib, pkg)
+function(libname, pkgname)
 {
    ## need to dyn.load sqlite.dll before we attempt to load
    ## RSQLite.dll  -- there's got to be a better way...
 
    if(.Platform$OS.type=="windows"){
-      if(!is.loaded(symbol.C("sqlite3_libversion")))
+      if(!is.loaded("sqlite3_libversion"))
          dyn.load(system.file("libs", "sqlite3.dll", package = "RSQLite"))
    }
-
-   library.dynam("RSQLite", pkg, lib)
-   library(methods, warn.conflicts = FALSE)
-   library(DBI, warn.conflicts = FALSE)
+   library.dynam("RSQLite", package=pkgname)
 }

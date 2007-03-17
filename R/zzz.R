f60a6223557412fe5a85e243616f9b0cd03c981b
sethf <sethf@dcde13d4-9b1b-0410-ac9e-ef07de68c835>
2007-03-17 16:37:51 +0000

    Add NAMESPACE for DBI and RSQLite
    git-svn-id: https://hedgehog.fhcrc.org/compbio/r-dbi/trunk@279 dcde13d4-9b1b-0410-ac9e-ef07de68c835

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,15 @@
-## $Id$
 ".First.lib" <- 
 function(libname, pkgname)
 {
+    require("methods")
    ## need to dyn.load sqlite.dll before we attempt to load
    ## RSQLite.dll  -- there's got to be a better way...
 
-   if(.Platform$OS.type=="windows"){
-      if(!is.loaded("sqlite3_libversion"))
-         dyn.load(system.file("libs", "sqlite3.dll", package = "RSQLite"))
-   }
-   library.dynam("RSQLite", package=pkgname)
+##    if(.Platform$OS.type=="windows"){
+##       if(!is.loaded("sqlite3_libversion"))
+##          dyn.load(system.file("libs", "sqlite3.dll", package = "RSQLite"))
+##    }
+##    library.dynam("RSQLite", package=pkgname)
 }
+
+.onLoad <- .First.lib

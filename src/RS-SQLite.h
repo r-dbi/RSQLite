0a84e5a8aacfe1c2f59964399d535fb054121893
sethf <sethf@dcde13d4-9b1b-0410-ac9e-ef07de68c835>
2007-07-16 23:34:49 +0000

    Update to SQLite 3.4.0, use SQLite amalgamation
    This patch updates the bundled SQLite version to 3.4.0 and introduces
    a new mechanism of including the bundled SQLite code.  Instead of
    building a separate SQLite lib and linking to it, we now use SQLite's
    amalgamation file which puts all of SQLite into a single C file.  This
    has an added benefit of simplifying the build process on Windows.
    
    
    
    git-svn-id: https://hedgehog.fhcrc.org/compbio/r-dbi/trunk@330 dcde13d4-9b1b-0410-ac9e-ef07de68c835

--- src/RS-SQLite.h
+++ src/RS-SQLite.h
@@ -24,8 +24,8 @@
 extern  "C" {
 #endif
 
-#ifdef WIN32
-#  include "windows/sqlite3.h"
+#ifdef RSQLITE_USE_BUNDLED_SQLITE
+#  include "sqlite/sqlite3.h"
 #else
 #  include <sqlite3.h>
 #endif
---
+++ src/sqlite-all.c
@@ -0,0 +1,3 @@
+#ifdef RSQLITE_USE_BUNDLED_SQLITE || WIN32
+#  include "sqlite/sqlite3.c"
+#endif

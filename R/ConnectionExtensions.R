05f621066e06f48e1b318975164b6a62a531bac3
hadley <h.wickham@gmail.com>
2013-10-31 18:30:02 -0400

    Document new generics

--- R/ConnectionExtensions.R
+++ R/ConnectionExtensions.R
@@ -1,3 +1,9 @@
+#' Generics for getting and sending prepared queries.
+#' 
+#' @param conn An \code{DBIConnection} object.
+#' @param statement A SQL string
+#' @param bind.data A data frame
+#' @param ... Other arguments used by methods
 #' @export
 setGeneric("dbSendPreparedQuery", 
   def = function(conn, statement, bind.data, ...) 
@@ -5,13 +11,20 @@ setGeneric("dbSendPreparedQuery",
   valueClass = "DBIResult"
 )
 
+#' @rdname dbSendPreparedQuery
 #' @export
 setGeneric("dbGetPreparedQuery", 
   def = function(conn, statement, bind.data, ...) 
     standardGeneric("dbGetPreparedQuery")
 )
 
+#' Generic for creating a new transaction.
+#' 
+#' See method documentation for details.
+#' 
 #' @export
+#' @param conn An \code{DBIConnection} object.
+#' @param ... Other arguments used by methods
 setGeneric("dbBeginTransaction", 
   def = function(conn, ...)
     standardGeneric("dbBeginTransaction"),

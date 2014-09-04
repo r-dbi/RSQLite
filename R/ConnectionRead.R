77c20ff03e2f3a4b18dbb06d04f95b0fefc54c08
hadley <h.wickham@gmail.com>
2014-09-04 11:21:25 -0500

    Tidy & inline sqliteReadTable. Closes #20

--- R/ConnectionRead.R
+++ R/ConnectionRead.R
@@ -14,30 +14,6 @@
 #' @param name a character string specifying a table name. SQLite table names 
 #'   are \emph{not} case sensitive, e.g., table names \code{ABC} and \code{abc} 
 #'   are considered equal.
-#' @param ... Ignored. Needed for compatibility with generic.
-#' @examples
-#' \dontrun{
-#' conn <- dbConnect("SQLite", dbname = "sqlite.db")
-#' if(dbExistsTable(con, "fuel_frame")){
-#'    dbRemoveTable(conn, "fuel_frame")
-#'    dbWriteTable(conn, "fuel_frame", fuel.frame)
-#' }
-#' if(dbExistsTable(conn, "RESULTS"))
-#'    dbWriteTable(conn, "RESULTS", results2000, append = TRUE)
-#' else
-#'    dbWriteTable(conn, "RESULTS", results2000)
-#' 
-#' }
-#' @export
-#' @rdname dbReadTable
-setMethod("dbReadTable",
-  signature = signature(conn = "SQLiteConnection", name = "character"),
-  definition = function(conn, name, ...) sqliteReadTable(conn, name, ...),
-  valueClass = "data.frame"
-)
-
-#' @export
-#' @rdname dbReadTable
 #' @param check.names If \code{TRUE}, the default, column names will be
 #'   converted to valid R identifiers.
 #' @param row.names A string or an index specifying the column in the DBMS table 
@@ -47,33 +23,46 @@ setMethod("dbReadTable",
 #' @param select.cols  A SQL statement (in the form of a character vector of 
 #'    length 1) giving the columns to select. E.g. "*" selects all columns, 
 #'    "x,y,z" selects three columns named as listed.
-sqliteReadTable <- function(con, name, row.names = "row_names", 
-                            check.names = TRUE, select.cols="*", ...) {
-  out <- dbGetQuery(con, paste("SELECT", select.cols, "FROM", name))
-
-  if(check.names)
-    names(out) <- make.names(names(out), unique = TRUE)
-  ## should we set the row.names of the output data.frame?
-  nms <- names(out)
-  j <- switch(mode(row.names),
-    character = if(row.names=="") 0 else
-      match(tolower(row.names), tolower(nms),
-        nomatch = if(missing(row.names)) 0 else -1),
-    numeric=, logical = row.names,
-    NULL = 0,
-    0)
-  if(as.numeric(j)==0)
-    return(out)
-  if(is.logical(j)) ## Must be TRUE
-    j <- match(row.names, tolower(nms), nomatch=0)
-  if(j<1 || j>ncol(out)){
-    warning("row.names not set on output data.frame (non-existing field)")
-    return(out)
+#' @export
+#' @examples
+#' con <- dbConnect(SQLite())
+#' dbWriteTable(con, "mtcars", mtcars)
+#' dbReadTable(con, "mtcars")
+#' 
+#' dbDisconnect(con)
+setMethod("dbReadTable", c("SQLiteConnection", "character"),
+  function(conn, name, row.names = "row_names", check.names = TRUE, 
+           select.cols = "*") {
+    out <- dbGetQuery(conn, paste("SELECT", select.cols, "FROM", name))
+    
+    if(check.names) {
+      names(out) <- make.names(names(out), unique = TRUE)
+    }
+    
+    ## should we set the row.names of the output data.frame?
+    nms <- names(out)
+    if (is.character(row.names)) {
+      row.names <- match(tolower(row.names), tolower(nms), nomatch = 0L)
+    } else {
+      row.names <- as.integer(row.names)
+    }
+    stopifnot(length(row.names) == 1)
+    
+    if (row.names == 0L) return(out)
+    
+    if(row.names < 1 || row.names > ncol(out)) {
+      warning("row.names not set (non-existing field)",
+        call. = FALSE)
+      return(out)
+    }
+    rnms <- as.character(out[[row.names]])
+    if (anyDuplicated(rnms)) {
+      warning("row.names not set (duplicate elements in field)",
+        call. = FALSE)
+    } else {
+      out <- out[, -row.names, drop = F]
+      row.names(out) <- rnms
+    }
+    out
   }
-  rnms <- as.character(out[,j])
-  if(all(!duplicated(rnms))){
-    out <- out[,-j, drop = F]
-    row.names(out) <- rnms
-  } else warning("row.names not set on output (duplicate elements in field)")
-  out
-}
+)

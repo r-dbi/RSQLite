a028da49683c00bb6600ec8516b04d4db929d6ff
hadley <h.wickham@gmail.com>
2014-09-05 09:59:48 -0500

    Add named argument to transaction functions. #41

--- R/ConnectionTransactions.R
+++ R/ConnectionTransactions.R
@@ -12,6 +12,8 @@ NULL
 #' 
 #' @param conn a \code{\linkS4class{SQLiteConnection}} object, produced by
 #'   \code{\link[DBI]{dbConnect}}
+#' @param name Supply a name to use a named savepoint. This allows you to
+#'   nest multiple transaction
 #' @return A boolean, indicating success or failure.
 #' @examples
 #' con <- dbConnect(SQLite(), ":memory:")
@@ -34,27 +36,47 @@ NULL
 #' dbCommit(con)
 #' dbGetQuery(con, "SELECT count(*) FROM arrests")[1, ]
 #' 
+#' # Named savepoints can be nested --------------------------------------------
+#' dbBegin(con, "a")
+#' dbBegin(con, "b")
+#' dbRollback(con, "b")
+#' dbCommit(con, "a")
+#' 
 #' dbDisconnect(con)
 #' @name transactions
 NULL
 
 #' @export
 #' @rdname transactions
-setMethod("dbBegin", "SQLiteConnection", function(conn) {
-  dbGetQuery(conn, "BEGIN")
+setMethod("dbBegin", "SQLiteConnection", function(conn, name = NULL) {
+  if (is.null(name)) {
+    dbGetQuery(conn, "BEGIN")  
+  } else {
+    dbGetQuery(conn, paste("SAVEPOINT ", name))
+  }
+  
   TRUE
 })
 
 #' @export
 #' @rdname transactions
-setMethod("dbCommit", "SQLiteConnection", function(conn) {
-  dbGetQuery(conn, "COMMIT")
+setMethod("dbCommit", "SQLiteConnection", function(conn, name = NULL) {
+  if (is.null(name)) {
+    dbGetQuery(conn, "COMMIT")
+  } else {
+    dbGetQuery(conn, paste("RELEASE SAVEPOINT ", name))
+  }
+  
   TRUE
 })
 
 #' @export
 #' @rdname transactions
-setMethod("dbRollback", "SQLiteConnection", function(conn) {
-  dbGetQuery(conn, "ROLLBACK")
+setMethod("dbRollback", "SQLiteConnection", function(conn, name = NULL) {
+  if (is.null(name)) {
+    dbGetQuery(conn, "ROLLBACK")
+  } else {
+    dbGetQuery(conn, paste("ROLLBACK TO ", name))
+  }
   TRUE
 })

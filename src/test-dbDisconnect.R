32185ab02d865943f0c2aab3f4b2af28c92cd9e0
hadley <h.wickham@gmail.com>
2014-09-07 17:25:04 -0500

    Convert connection_gc_test to testthat

--- src/test-dbDisconnect.R
+++ src/test-dbDisconnect.R
@@ -6,3 +6,13 @@ test_that("closes result set with warning", {
   
   expect_warning(dbDisconnect(con), "closing automatically")
 })
+
+test_that("connections are finalised", {
+  dbs <- lapply(1:50, function(x) {
+    dbConnect(SQLite(), dbname = ":memory:")
+  })
+  expect_equal(dbGetInfo(SQLite())$num_con, 50)
+
+  rm(dbs); gc()
+  expect_equal(dbGetInfo(SQLite())$num_con, 0)
+}) 

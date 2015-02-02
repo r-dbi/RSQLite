9a82897ca16ec806e09a6f903f51e87f2d77dc76
JJ Allaire <jj@rstudio.org>
2015-02-02 17:01:59 -0500

    Remove XPtr boilerplate
    Remove boilerplate checking code as these checks are now automatically performed by Rcpp.
    
    Note this relies on code in a branch not yet merged into Rcpp: https://github.com/RcppCore/Rcpp/tree/feature/xptr-improvements

--- src/result.cpp
+++ src/result.cpp
@@ -4,24 +4,17 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 XPtr<SqliteResult> rsqlite_send_query(XPtr<SqliteConnectionPtr> con, std::string sql) {
-  if (R_ExternalPtrAddr(con) == NULL) stop("Connection expired");
-
   SqliteResult* res = new SqliteResult((*con), sql);
   return XPtr<SqliteResult>(res, true);
 }
 
 // [[Rcpp::export]]
 void rsqlite_clear_result(XPtr<SqliteResult> res) {
-  if (R_ExternalPtrAddr(res) == NULL) stop("Results already closed");
-  
-  delete (SqliteResult*) res;
-  R_ClearExternalPtr(res);
+  res.release();
 }
 
 // [[Rcpp::export]]
 List rsqlite_fetch(XPtr<SqliteResult> res, int n = 10) {
-  if (R_ExternalPtrAddr(res) == NULL) stop("Results expired");
-  
   if (n < 0) {
     return res->fetch_all();  
   } else {
@@ -31,41 +24,31 @@ List rsqlite_fetch(XPtr<SqliteResult> res, int n = 10) {
 
 // [[Rcpp::export]]
 void rsqlite_bind_params(XPtr<SqliteResult> res, List params) {
-  if (R_ExternalPtrAddr(res) == NULL) stop("Results expired");
-  
   res->bind(params);
 }
 
 
 // [[Rcpp::export]]
 bool rsqlite_has_completed(XPtr<SqliteResult> res) {
-  if (R_ExternalPtrAddr(res) == NULL) stop("Results expired");
-  
   return res->complete();
 }
 
 // [[Rcpp::export]]
 int rsqlite_row_count(XPtr<SqliteResult> res) {
-  if (R_ExternalPtrAddr(res) == NULL) stop("Results expired");
-  
   return res->nrows() - 1;
 }
 
 // [[Rcpp::export]]
 int rsqlite_rows_affected(XPtr<SqliteResult> res) {
-  if (R_ExternalPtrAddr(res) == NULL) stop("Results closed");
-  
   return res->rows_affected();
 }
 
 // [[Rcpp::export]]
 List rsqlite_column_info(XPtr<SqliteResult> res) {
-  if (R_ExternalPtrAddr(res) == NULL) stop("Results expired");
-  
   return res->column_info();
 }
 
 // [[Rcpp::export]]
 bool rsqlite_result_valid(XPtr<SqliteResult> res) {
-  return R_ExternalPtrAddr(res) != NULL;
+  return res;
 }

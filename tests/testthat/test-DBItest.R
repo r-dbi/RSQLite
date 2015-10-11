a6549b9643242feade9cc5b66c2a983379feec9b
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-10-11 09:59:07 +0200

    add ignores for time and timestamp

--- tests/testthat/test-DBItest.R
+++ tests/testthat/test-DBItest.R
@@ -1,11 +1,14 @@
 DBItest::make_context(SQLite(), NULL)
 DBItest::test_all(c(
-  "package_dependencies", # #102
-  "constructor_strict",   # relaxed constructor check still active
-  "show",                 # rstats-db/RPostgres#49
-  "get_info",             # to be discussed
-  "data_logical",         # not an error, no logical data type
-  "data_64_bit",          # rstats-db/RPostgres#51
-  "data_date",            # #103
+  "package_dependencies",  # #102
+  "constructor_strict",    # relaxed constructor check still active
+  "show",                  # rstats-db/RPostgres#49
+  "get_info",              # to be discussed
+  "data_logical",          # not an error, no logical data type
+  "data_64_bit",           # rstats-db/RPostgres#51
+  "data_date",             # #103
+  "data_time",             # syntax not supported
+  "data_timestamp",        # syntax not supported
+  "data_timestamp_parens", # #104
   NULL
 ))

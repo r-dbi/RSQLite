ac45511a5c35a9035947cfc1a9dbaf19a16bb59a
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-11-14 19:18:15 +0100

    DBItest: bind_*_named_*

--- tests/testthat/test-dbSendQuery.R
+++ tests/testthat/test-dbSendQuery.R
@@ -1,16 +1,5 @@
 context("dbSendQuery")
 
-test_that("simple named binding works", {
-  con <- dbConnect(SQLite(), ":memory:")
-  dbWriteTable(con, "t1", data.frame(x = 1, y = 2))
-  
-  dbSendQuery(con, "INSERT INTO t1 VALUES ($x, $y)", list(y = 1, x = 2))
-  expect_equal(dbReadTable(con, "t1")$x, c(1, 2)) 
-  
-  dbSendQuery(con, "INSERT INTO t1 VALUES (:x, :y)", list(y = 3, x = 4))
-  expect_equal(dbReadTable(con, "t1")$x, c(1, 2, 4)) 
-})
-
 test_that("bound params must match query params", {
   con <- datasetsDb()
   rs <- dbSendQuery(con, "SELECT * FROM mtcars WHERE cyl = $x")

f99427f90dceca52a45109e3efeb2cdf841b627b
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-11-14 19:19:13 +0100

    DBItest: bind_raw_*

--- tests/testthat/test-dbSendQuery.R
+++ tests/testthat/test-dbSendQuery.R
@@ -8,14 +8,3 @@ test_that("bound params must match query params", {
   expect_error(dbBind(rs, list(y = 1)), "No parameter")
   expect_error(dbBind(rs, list(quote(a))), "type symbol")
 })
-
-test_that("can round trip raw vector", {
-  con <- dbConnect(RSQLite::SQLite())
-  
-  dbGetQuery(con, "CREATE TABLE test1 (x BLOB)")
-  rs <- dbSendQuery(con, "INSERT INTO test1 VALUES (:x)")
-  dbBind(rs, list(x = list(serialize("abc", NULL))))
-  
-  x <- dbGetQuery(con, "SELECT * FROM test1")
-  expect_equal(unserialize(x$x[[1]]), "abc")
-})

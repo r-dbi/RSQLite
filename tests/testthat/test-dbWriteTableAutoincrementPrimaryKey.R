afe1682c2e4519c5262a21d21cd5b493d0981b62
Will Beasley <wibeasley@hotmail.com>
2015-11-15 16:12:58 -0600

    Refactor auto-increment
    A test only declares input & expected output.  The helper function does the real work.

--- tests/testthat/test-dbWriteTableAutoincrementPrimaryKey.R
+++ tests/testthat/test-dbWriteTableAutoincrementPrimaryKey.R
@@ -1,154 +1,114 @@
 context("dbWriteTable Autoincrement with a Primary Key")
 
-# Helper Testing Function
-ddl <- "CREATE TABLE `tbl` (
+# Helper variable and function
+sql_ddl <- "CREATE TABLE `tbl` (
   `id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	`name`  TEXT    NOT NULL UNIQUE,
 	`score` INTEGER NOT NULL
 );"
 
-create_populated_table <- function( d_local ) {
+create_and_compare_table <- function( d_local, expected_remote_id ) {
+  #Create a connection, create a table, and populate the table.
   con <- dbConnect(SQLite())
-  dbSendQuery(con, ddl)  
+  dbSendQuery(con, sql_ddl)  
   dbWriteTable(con, name = 'tbl', value = d_local, append = TRUE, row.names = FALSE)
   
-  ds_remote <- dbReadTable(con, "tbl")
-  ds_remote <- ds_remote[order(ds_remote$score), ] #Sort returned data.frame so comparisons are more robust.
-  return( ds_remote )
+  #Reads from the database and sort so comparisons are more robust.
+  d_remote <- dbReadTable(con, "tbl")
+  d_remote <- d_remote[order(d_remote$score), ]
+  
+  #Compares actual to expected values.
+  expect_equal(d_remote$name,  d_local$name)
+  expect_equal(d_remote$score, d_local$score)
+  expect_equal(d_remote$id,    expected_remote_id, label = "The autoincrement values should be assigned correctly.")
 }
 
-test_that("autoincrement correctly populated by database", {
-  ds_local <- data.frame(
-    #Notice the 'id' column isn't set locally, only remotely.
-    name             = letters,
-    score            = 1:26,
-    stringsAsFactors = FALSE
-  )
-  
-  ds_remote <- create_populated_table(ds_local)
-
-  expect_equal(ds_remote$name,  ds_local$name)
-  expect_equal(ds_remote$score, ds_local$score)
-  expect_equal(ds_remote$id, ds_remote$score, label = "The autoincrement values should be assigned correctly.")
-})
-
 test_that("autoincrement column not present before sending to database", {
+  #The column is created and populated in the DB.
   ds_local <- data.frame(
-    # Notice the 'id' column is not declared
-    # id             = NA_integer_,
+    # id             = <not created>, # The 'id' column is not declared in R.
     name             = letters,
     score            = 1:26,
     stringsAsFactors = FALSE
   )
-  
-  ds_remote <- create_populated_table(ds_local)
-  
-  expect_equal(ds_remote$name,  ds_local$name)
-  expect_equal(ds_remote$score, ds_local$score)
-  expect_equal(ds_remote$id, ds_local$score, label = "The autoincrement values should be assigned correctly.")
+  expected_ids <- 1:26
+  create_and_compare_table(ds_local, expected_ids)
 })
 
 test_that("autoincrement populated before database with integers", {
+  #The id column is set locally, which prevents/overrides the autoincrement assignment in the DB.
   ds_local <- data.frame(
-    #Notice the 'id' column is set locally, which overrides the autoincrement assignment in the DB.
     id               = 126:101,
     name             = letters,
     score            = 1:26,
     stringsAsFactors = FALSE
   )
-  
-  ds_remote <- create_populated_table(ds_local)
-  ds_local  <- ds_local[order(ds_local$score), ]
-  
-  expect_equal(ds_remote$name,  ds_local$name)
-  expect_equal(ds_remote$score, ds_local$score)
-  expect_equal(ds_remote$id, 100 + rev(ds_remote$score), label = "The autoincrement values should be assigned correctly.")
+  expected_ids <- 126:101
+  create_and_compare_table(ds_local, expected_ids)
 })
 
 test_that("autoincrement populated before database with all NAs", {
+  #The id column is set to NAs, and replaced by an incremented value in the DB.
   ds_local <- data.frame(
-    #Notice the 'id' column is set locally, which overrides the autoincrement assignment in the DB.
     id               = NA_integer_,
     name             = letters,
     score            = 1:26,
     stringsAsFactors = FALSE
   )
-  
-  ds_remote <- create_populated_table(ds_local)
-  
-  expect_equal(ds_remote$name,  ds_local$name)
-  expect_equal(ds_remote$score, ds_local$score)
-  expect_equal(ds_remote$id, ds_remote$score, label = "The autoincrement values should be assigned correctly.")
+  expected_ids <- 1:26
+  create_and_compare_table(ds_local, expected_ids)
 })
 
 test_that("autoincrement populated before database with some NAs in sequential order", {
+  #The NA holes are assigned an autoincrementing value in the DB.
   ds_local <- data.frame(
-    #Notice the 'id' column is set locally, which overrides the autoincrement assignment in the DB.
     id               = c(101, 102, NA_integer_, 204, NA_integer_, 306, NA_integer_),
     name             = letters[1:7],
     score            = 1:7,
     stringsAsFactors = FALSE
   )
-  
-  ds_remote <- create_populated_table(ds_local)
   expected_ids <- c(101, 102, 103, 204, 205, 306, 307) #Notice the jumps to 204 and to 306.
-  
-  expect_equal(ds_remote$name,  ds_local$name)
-  expect_equal(ds_remote$score, ds_local$score)
-  expect_equal(ds_remote$id, expected_ids, label = "The autoincrement values should be assigned correctly.")
+  create_and_compare_table(ds_local, expected_ids)
 })
 
 test_that("autoincrement populated before database with some NAs in reverse order", {
   # The id assignment in the local dataset is (mostly) reverse ordered.
   #   This test verifies the database assignment doesn't duplicate previous ID values.
   ds_local <- data.frame(
-    #Notice the 'id' column is set locally, which overrides the autoincrement assignment in the DB.
     id               = c(NA_integer_, 306, NA_integer_, -204, 103, 102, NA_integer_),
     name             = letters[1:7],
     score            = 1:7,
     stringsAsFactors = FALSE
   )
-  
-  ds_remote <- create_populated_table(ds_local)
   expected_ids <- c(1, 306, 307, -204, 103, 102, 308) #The last value is `308`, not a duplicate `103`.
-  
-  expect_equal(ds_remote$name,  ds_local$name)
-  expect_equal(ds_remote$score, ds_local$score)
-  expect_equal(ds_remote$id, expected_ids, label = "The autoincrement values should be assigned correctly.")
+  create_and_compare_table(ds_local, expected_ids)
 })
 
 test_that("autoincrement partially populated before database with some all negative integers", {
   # The id assignment in the local dataset is either a missing number, or a negative number.
   #   This test verifies the database assignment doesn't assign a negative value, even if it would be the next largest value.
   ds_local <- data.frame(
-    #Notice the 'id' column is set locally, which overrides the autoincrement assignment in the DB.
     id               = c(-9, -8, NA_integer_, -6, -4, -3, NA_integer_),
     name             = letters[1:7],
     score            = 1:7,
     stringsAsFactors = FALSE
   )
-  
-  ds_remote <- create_populated_table(ds_local)
   expected_ids <- c(-9, -8, 1, -6, -4, -3, 2)
-  
-  expect_equal(ds_remote$name,  ds_local$name)
-  expect_equal(ds_remote$score, ds_local$score)
-  expect_equal(ds_remote$id, expected_ids, label = "The autoincrement values should be assigned correctly.")
+  create_and_compare_table(ds_local, expected_ids)
 })
 
 test_that("autoincrement partially populated with duplicate IDs throws an error", {
   # The id assignment in the local dataset is either a missing number, or duplicated values.
-  #   This test verifies the database assignment doesn't implicitly override the duplicates with a unique value.
-  con <- dbConnect(SQLite())
+  #   This test verifies the database assignment doesn't implicitly/silently override the duplicates with a unique value.
   ds_local <- data.frame(
-    #Notice the 'id' column is set locally, which overrides the autoincrement assignment in the DB.
     id               = c(1, 1, NA_integer_, NA_integer_, 3, 3, 3),
     name             = letters[1:7],
     score            = 1:7,
     stringsAsFactors = FALSE
   )
   
-  dbSendQuery(con, ddl)  
+  con <- dbConnect(SQLite())
+  dbSendQuery(con, sql_ddl)  
   expect_error(
     dbWriteTable(con, name = 'tbl', value = ds_local, append = TRUE, row.names = FALSE),
     "Error : UNIQUE constraint failed: tbl\\.id\n"

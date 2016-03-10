3aeb6d88488a3ed32e44780bcbf35536d6eaaaf3
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-03-10 10:48:11 +0100

    issue fixed

--- tests/testthat/test-DBItest.R
+++ tests/testthat/test-DBItest.R
@@ -32,7 +32,6 @@ DBItest::test_all(c(
   "append_table_error",                         # #112
   "temporary_table",                            # #113
   "quote_identifier_not_vectorized",            # rstats-db/DBI#24
-  "roundtrip_keywords",                         # #106
   "roundtrip_quotes",                           # #107
   "roundtrip_logical",                          # not an error, no logical data type
   "roundtrip_64_bit",                           # not an error, loose typing

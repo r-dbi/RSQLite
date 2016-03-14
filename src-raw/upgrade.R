fd2fa2895972c602416ab6e423dfc8d971eb2f69
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-03-15 00:28:50 +0100

    slightly better upgrade

--- src-raw/upgrade.R
+++ src-raw/upgrade.R
@@ -1,11 +1,15 @@
+library(magrittr)
+
 html <- xml2::read_html("https://www.sqlite.org/download.html")
 
 latest_name <- html %>%
   rvest::html_nodes("a") %>%
-  xml2::xml_find_all('//*[(@id = "a1")]') %>% 
-  rvest::html_text
+  xml2::xml_find_all('//*[(@id = "a1")] | //*[(@id = "a2")]') %>%
+  rvest::html_text() %>%
+  grep("amalgamation", ., value = TRUE) %>%
+  .[1]
 
-latest <- paste0("http://www.sqlite.org/2015/", latest_name)
+latest <- paste0("http://www.sqlite.org/", Sys.Date() %>% strftime("%Y"), "/", latest_name)
 tmp <- tempfile()
 download.file(latest, tmp)
 unzip(tmp, exdir = "src/", junkpaths = TRUE)

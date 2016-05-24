910529fe5f8832675de257c43c9db2741706ba26
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-05-24 17:08:10 +0200

    add test
    unclear: why is the gc() at the end necessary to avoid the warning about open result sets?

---
+++ tests/testthat/test-error.R
@@ -0,0 +1,33 @@
+context("error")
+
+test_that("parameters with length != 1 (#89)", {
+  con <- dbConnect(SQLite())
+  on.exit(dbDisconnect(con), add = TRUE)
+
+  dbGetQuery(con, "CREATE TABLE records(x REAL, y REAL);")
+  expect_error(
+    dbGetQuery(
+      con,
+      "INSERT INTO records (x, y) VALUES (:x, :y)",
+      params = list(x = 1, y = 1)
+    ),
+    NA)
+  expect_error(
+    dbGetQuery(
+      con,
+      "INSERT INTO records (x, y) VALUES (:x, :y)",
+      params = list(x = 1, y = list(1,2))
+    ),
+    "Parameter 2 does not have length 1")
+  expect_error(
+    dbGetQuery(
+      con,
+      "INSERT INTO records (x, y) VALUES (:x, :y)",
+      params = list(x = 1, y = NULL)
+    ),
+    "Parameter 2 does not have length 1")
+
+  # Why is this necessary???
+  gc()
+
+})

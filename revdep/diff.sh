ab4ca0995c2b4ad2c9e7af996ce52e7e89e75e5c
Kirill Müller <krlmlr+r@mailbox.org>
2016-09-01 11:03:24 +0200

    need to commit intermediate states to record SHA1 of commit

--- revdep/diff.sh
+++ revdep/diff.sh
@@ -7,7 +7,7 @@ branch=f-revdep
 
 cd $(dirname $0)/..
 git checkout -- .
-git clean -fdx
+git clean --force -dx
 
 git checkout $branch --
 cp -r revdep revdep-
@@ -17,10 +17,14 @@ rm -rf revdep
 mv revdep- revdep
 rm -rf revdep/install
 
+git add revdep
+git commit -m "revdep update for clean state" || true
+
 R -f revdep/check.R
 #echo $old_tag >> revdep/README.md
 
 mv revdep revdep-$old_tag
+git checkout .
 
 git checkout $branch --
 
@@ -29,8 +33,10 @@ mv revdep-$old_tag revdep
 git add revdep
 git commit -m "revdep update with $old_tag results"
 
-git clean -fdx
+git clean --force -dx
 rm -rf revdep/install
+git add revdep
+git commit -m "revdep update for clean state" || true
 
 R -f revdep/check.R
 #echo $branch >> revdep/README.md

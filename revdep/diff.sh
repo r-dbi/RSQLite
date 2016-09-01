77298a00f43ec8d8a11d1b3db67c54359abc1eaf
Kirill Müller <krlmlr+r@mailbox.org>
2016-09-01 19:01:45 +0000

    copy versioned README and problems

--- revdep/diff.sh
+++ revdep/diff.sh
@@ -30,6 +30,8 @@ git checkout $branch --
 
 rm -rf revdep
 mv revdep-$old_tag revdep
+cp revdep/README.md revdep/README-${old_tag}.md
+cp revdep/problems.md revdep/problems-${old_tag}.md
 git add revdep
 git commit -m "revdep update with $old_tag results"
 
@@ -40,6 +42,9 @@ git commit -m "revdep update for clean state" || true
 
 R -f revdep/check.R
 #echo $branch >> revdep/README.md
+
+cp revdep/README.md revdep/README-${branch}.md
+cp revdep/problems.md revdep/problems-${branch}.md
 
 git add revdep
 git commit -m "revdep update with $branch results"

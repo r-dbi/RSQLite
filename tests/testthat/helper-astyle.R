ff95e0f9d4e4dcc90fd011f2f09be5f5e4167a8e
Kirill Müller <krlmlr+r@mailbox.org>
2016-09-01 22:09:44 +0200

    astyle

---
+++ tests/testthat/helper-astyle.R
@@ -0,0 +1,27 @@
+astyle <- function(extra_args = character()) {
+  astyle_cmd <- "astyle"
+  if (Sys.which(astyle_cmd) == "") {
+    skip("astyle not found")
+  }
+
+  astyle_args <- c(
+    "-n",
+    "--indent=spaces=2",
+    "--indent-namespaces",
+    "--indent-preproc-block",
+    "--unpad-paren",
+    "--pad-header",
+    "--min-conditional-indent=0",
+    "--align-pointer=type",
+    "--align-reference=type"
+  )
+
+  src_path <- normalizePath(testthat::test_path("../../src"))
+  src_files <- dir(src_path, "[.](?:cpp|h)$", recursive = FALSE, full.names = TRUE)
+  astyle_files <- grep("(?:RcppExports[.]cpp)", src_files, value = TRUE, invert = TRUE)
+  output <- system2(astyle_cmd, c(astyle_args, astyle_files, extra_args), stdout = TRUE, stderr = TRUE)
+  unchanged <- grepl("^Unchanged", output)
+  if (any(!unchanged)) {
+    warning(paste(output[!unchanged], collapse = "\n"))
+  }
+}

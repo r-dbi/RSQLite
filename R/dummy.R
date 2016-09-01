6e7ccad81d865a4706a1d59f01d39b258825627f
Kirill Müller <krlmlr+r@mailbox.org>
2016-09-01 16:02:56 +0200

    fix alias

--- R/dummy.R
+++ R/dummy.R
@@ -7,7 +7,7 @@
 
 
 #' @rdname dummy-methods
-#' @aliases dbGetQuery,NULL-method
+#' @aliases dbGetQuery,NULL,ANY-method
 #' @inheritParams DBI::dbGetQuery
 #' @export
 setMethod("dbGetQuery", "NULL", function(conn, statement, ...) {

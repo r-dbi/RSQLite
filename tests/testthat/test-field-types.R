b4f52d5dd4a39d4c254c5d5057cb5ae6bca90151
Kirill Müller <krlmlr+r@mailbox.org>
2016-09-09 22:05:55 +0200

    add test working in v1.0.0

---
+++ tests/testthat/test-field-types.R
@@ -0,0 +1,46 @@
+context("field-types")
+
+test_that("passing field.types", {
+  con <- dbConnect(SQLite())
+  on.exit(dbDisconnect(con))
+  
+  dbWriteTable(con, "a", data.frame(a = 1:3), field.types = c("a" = "TEXT"))
+  res <- dbReadTable(con, "a")
+  
+  expect_identical(res, data.frame(a = c("1", "2", "3"), stringsAsFactors = FALSE))
+})
+
+test_that("passing field.types for some columns only", {
+  con <- dbConnect(SQLite())
+  on.exit(dbDisconnect(con))
+  
+  expect_error(dbWriteTable(con, "a", data.frame(a = 1:3, b = 4:6), field.types = c("a" = "TEXT")))
+})
+
+test_that("passing field.types with wrong name", {
+  con <- dbConnect(SQLite())
+  on.exit(dbDisconnect(con))
+  
+  dbWriteTable(con, "a", data.frame(a = 1:3), field.types = c("b" = "TEXT"))
+  res <- dbReadTable(con, "a")
+  
+  expect_identical(res, data.frame(b = c("1", "2", "3"), stringsAsFactors = FALSE))
+})
+
+test_that("passing field.types with primary key information", {
+  con <- dbConnect(SQLite())
+  on.exit(dbDisconnect(con))
+  
+  dbWriteTable(con, "a", data.frame(a = 1:3), field.types = c("a" = "INTEGER PRIMARY KEY"))
+  res <- dbReadTable(con, "a")
+  
+  expect_identical(res, data.frame(a = 1:3))
+})
+
+test_that("passing field.types with primary key information and non-unique values", {
+  con <- dbConnect(SQLite())
+  on.exit(dbDisconnect(con))
+  
+  expect_error(dbWriteTable(con, "a", data.frame(a = c(1, 2, 1)), field.types = c("a" = "INTEGER PRIMARY KEY")),
+               "UNIQUE")
+})

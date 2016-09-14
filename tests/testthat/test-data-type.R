9f04f740ec39e2580ed13a408304271b6dcb5921
Kirill Müller <krlmlr+r@mailbox.org>
2016-09-14 10:33:15 +0200

    add missing tests from v1.0.0

--- tests/testthat/test-data-type.R
+++ tests/testthat/test-data-type.R
@@ -1,5 +1,9 @@
 context("Data type convertion")
 
+sqliteDataType <- function(x) {
+  dbDataType(SQLite(), x)
+}
+
 # Specific to RSQLite
 test_that("integers and boolean stored as INTEGER", {
   expect_equal(dbDataType(SQLite(), 1L), "INTEGER")
@@ -16,6 +20,8 @@ test_that("doubles stored as REAL", {
 # Specific to RSQLite
 test_that("character is stored as TEXT", {
   expect_equal(dbDataType(SQLite(), "a"), "TEXT")
+  expect_equal(dbDataType(SQLite(), factor(letters)), "TEXT")
+  expect_equal(dbDataType(SQLite(), ordered(letters)), "TEXT")
 })
 
 # Specific to RSQLite
@@ -29,4 +35,30 @@ test_that("raw and list stored as BLOB", {
 test_that("dates are stored as REAL", {
   expect_equal(dbDataType(SQLite(), Sys.Date()), "REAL")
   expect_equal(dbDataType(SQLite(), Sys.time()), "REAL")
+})
+
+# Tested by DBItest
+test_that("AsIs class is ignored", {
+  df <- data.frame(
+    a = I(1:2),
+    b = I(c("x", "y")),
+    c = I(list(raw(3), raw(1))),
+    d = I(c(1.1, 2.2))
+  )
+  got <- sapply(df, sqliteDataType)
+  expect_equal(got, c(a="INTEGER", b="TEXT", c="BLOB", d="REAL"))
+})
+
+test_that("unknown classes default to storage.mode()", {
+  expect_equal(sqliteDataType(Sys.Date()), "REAL")
+  expect_equal(sqliteDataType(Sys.time()), "REAL")
+
+  intClass <- structure(1L, class="unknown1")
+  expect_equal(sqliteDataType(intClass), "INTEGER")
+
+  dblClass <- structure(3.14, class="unknown1")
+  expect_equal(sqliteDataType(dblClass), "REAL")
+
+  strClass <- structure("abc", class="unknown1")
+  expect_equal(sqliteDataType(strClass), "TEXT")
 })

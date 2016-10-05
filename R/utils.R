e369fe6c5b00de1b19836506b390555335074e7f
Kirill Müller <krlmlr+r@mailbox.org>
2016-10-05 11:13:43 +0200

    warning_once()

--- R/utils.R
+++ R/utils.R
@@ -5,3 +5,13 @@ vlapply <- function(X, FUN, ..., USE.NAMES = TRUE) {
 vcapply <- function(X, FUN, ..., USE.NAMES = TRUE) {
   vapply(X = X, FUN = FUN, FUN.VALUE = character(1L), ..., USE.NAMES = USE.NAMES)
 }
+
+stopc <- function(...) {
+  stop(..., call. = FALSE, domain = NA)
+}
+
+warningc <- function(...) {
+  warning(..., call. = FALSE, domain = NA)
+}
+
+warning_once <- memoise::memoise(warningc)

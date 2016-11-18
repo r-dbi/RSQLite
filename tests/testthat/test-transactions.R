24028e3102a68b088b22fdc3471e43affdd201ef
Kirill Müller <krlmlr+r@mailbox.org>
2016-11-18 23:33:04 +0100

    test named transactions

--- tests/testthat/test-transactions.R
+++ tests/testthat/test-transactions.R
@@ -77,3 +77,170 @@ test_that("no nested unnamed transactions (rollback after error)", {
   expect_equal(dbListTables(con), "a")
   expect_equal(dbListTables(con2), "a")
 })
+
+test_that("commit named transactions", {
+  db_file <- tempfile("transactions", fileext = ".sqlite")
+  con <- dbConnect(SQLite(), db_file)
+  con2 <- dbConnect(SQLite(), db_file)
+  on.exit({dbDisconnect(con); dbDisconnect(con2)}, add = TRUE)
+
+  dbBegin(con, name = "tx")
+  dbWriteTable(con, "a", data.frame(a=1))
+  expect_equal(dbListTables(con), "a")
+  expect_equal(dbListTables(con2), character())
+  dbCommit(con, name = "tx")
+  expect_equal(dbListTables(con), "a")
+  expect_equal(dbListTables(con2), "a")
+
+  dbWriteTable(con, "b", data.frame(a=1))
+  expect_equal(dbListTables(con), c("a", "b"))
+  expect_equal(dbListTables(con2), c("a", "b"))
+})
+
+test_that("rollback named transactions", {
+  db_file <- tempfile("transactions", fileext = ".sqlite")
+  con <- dbConnect(SQLite(), db_file)
+  con2 <- dbConnect(SQLite(), db_file)
+  on.exit({dbDisconnect(con); dbDisconnect(con2)}, add = TRUE)
+
+  dbBegin(con, name = "tx")
+  dbWriteTable(con, "a", data.frame(a=1))
+  expect_equal(dbListTables(con), "a")
+  expect_equal(dbListTables(con2), character())
+
+  dbRollback(con, name = "tx")
+  expect_equal(dbListTables(con), character())
+  expect_equal(dbListTables(con2), character())
+
+  dbWriteTable(con, "b", data.frame(a=1))
+  expect_equal(dbListTables(con), "b")
+  expect_equal(dbListTables(con2), "b")
+})
+
+test_that("nested named transactions (commit - commit)", {
+  db_file <- tempfile("transactions", fileext = ".sqlite")
+  con <- dbConnect(SQLite(), db_file)
+  con2 <- dbConnect(SQLite(), db_file)
+  on.exit({dbDisconnect(con); dbDisconnect(con2)}, add = TRUE)
+
+  dbBegin(con, name = "tx")
+  dbWriteTable(con, "a", data.frame(a=1))
+  expect_equal(dbListTables(con), "a")
+  expect_equal(dbListTables(con2), character())
+
+  dbBegin(con, name = "tx2")
+  expect_equal(dbListTables(con), "a")
+  expect_equal(dbListTables(con2), character())
+
+  dbWriteTable(con, "b", data.frame(a=1))
+  expect_equal(dbListTables(con), c("a", "b"))
+  expect_equal(dbListTables(con2), character())
+
+  dbCommit(con, name = "tx2")
+  expect_equal(dbListTables(con), c("a", "b"))
+  expect_equal(dbListTables(con2), character())
+
+  dbCommit(con, name = "tx")
+  expect_equal(dbListTables(con), c("a", "b"))
+  expect_equal(dbListTables(con2), c("a", "b"))
+
+  dbWriteTable(con, "c", data.frame(a=1))
+  expect_equal(dbListTables(con), c("a", "b", "c"))
+  expect_equal(dbListTables(con2), c("a", "b", "c"))
+})
+
+test_that("nested named transactions (commit - rollback)", {
+  db_file <- tempfile("transactions", fileext = ".sqlite")
+  con <- dbConnect(SQLite(), db_file)
+  con2 <- dbConnect(SQLite(), db_file)
+  on.exit({dbDisconnect(con); dbDisconnect(con2)}, add = TRUE)
+
+  dbBegin(con, name = "tx")
+  dbWriteTable(con, "a", data.frame(a=1))
+  expect_equal(dbListTables(con), "a")
+  expect_equal(dbListTables(con2), character())
+
+  dbBegin(con, name = "tx2")
+  expect_equal(dbListTables(con), "a")
+  expect_equal(dbListTables(con2), character())
+
+  dbWriteTable(con, "b", data.frame(a=1))
+  expect_equal(dbListTables(con), c("a", "b"))
+  expect_equal(dbListTables(con2), character())
+
+  dbCommit(con, name = "tx2")
+  expect_equal(dbListTables(con), c("a", "b"))
+  expect_equal(dbListTables(con2), character())
+
+  dbRollback(con, name = "tx")
+  expect_equal(dbListTables(con), character())
+  expect_equal(dbListTables(con2), character())
+
+  dbWriteTable(con, "c", data.frame(a=1))
+  expect_equal(dbListTables(con), "c")
+  expect_equal(dbListTables(con2), "c")
+})
+
+test_that("nested named transactions (rollback - commit)", {
+  db_file <- tempfile("transactions", fileext = ".sqlite")
+  con <- dbConnect(SQLite(), db_file)
+  con2 <- dbConnect(SQLite(), db_file)
+  on.exit({dbDisconnect(con); dbDisconnect(con2)}, add = TRUE)
+
+  dbBegin(con, name = "tx")
+  dbWriteTable(con, "a", data.frame(a=1))
+  expect_equal(dbListTables(con), "a")
+  expect_equal(dbListTables(con2), character())
+
+  dbBegin(con, name = "tx2")
+  expect_equal(dbListTables(con), "a")
+  expect_equal(dbListTables(con2), character())
+
+  dbWriteTable(con, "b", data.frame(a=1))
+  expect_equal(dbListTables(con), c("a", "b"))
+  expect_equal(dbListTables(con2), character())
+
+  dbRollback(con, name = "tx2")
+  expect_equal(dbListTables(con), "a")
+  expect_equal(dbListTables(con2), character())
+
+  dbCommit(con, name = "tx")
+  expect_equal(dbListTables(con), "a")
+  expect_equal(dbListTables(con2), "a")
+
+  dbWriteTable(con, "c", data.frame(a=1))
+  expect_equal(dbListTables(con), c("a", "c"))
+  expect_equal(dbListTables(con2), c("a", "c"))
+})
+
+test_that("nested named transactions (rollback - rollback)", {
+  db_file <- tempfile("transactions", fileext = ".sqlite")
+  con <- dbConnect(SQLite(), db_file)
+  con2 <- dbConnect(SQLite(), db_file)
+  on.exit({dbDisconnect(con); dbDisconnect(con2)}, add = TRUE)
+
+  dbBegin(con, name = "tx")
+  dbWriteTable(con, "a", data.frame(a=1))
+  expect_equal(dbListTables(con), "a")
+  expect_equal(dbListTables(con2), character())
+
+  dbBegin(con, name = "tx2")
+  expect_equal(dbListTables(con), "a")
+  expect_equal(dbListTables(con2), character())
+
+  dbWriteTable(con, "b", data.frame(a=1))
+  expect_equal(dbListTables(con), c("a", "b"))
+  expect_equal(dbListTables(con2), character())
+
+  dbRollback(con, name = "tx2")
+  expect_equal(dbListTables(con), "a")
+  expect_equal(dbListTables(con2), character())
+
+  dbRollback(con, name = "tx")
+  expect_equal(dbListTables(con), character())
+  expect_equal(dbListTables(con2), character())
+
+  dbWriteTable(con, "c", data.frame(a=1))
+  expect_equal(dbListTables(con), "c")
+  expect_equal(dbListTables(con2), "c")
+})

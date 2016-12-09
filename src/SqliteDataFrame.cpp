a63cd1cfd0b29f7e8233e2e1807e5a3563b5d694
Kirill Müller <krlmlr+r@mailbox.org>
2016-12-09 22:25:22 +0100

    need to protect column for adding blobs
    because this in itself can trigger an allocation and a garbage collection

--- src/SqliteDataFrame.cpp
+++ src/SqliteDataFrame.cpp
@@ -32,7 +32,7 @@ bool SqliteDataFrame::set_col_values() {
   }
 
   for (int j = 0; j < out.length(); ++j) {
-    SEXP col = out[j];
+    RObject col = out[j];
     set_col_value(col, j);
     out[j] = col;
   }
@@ -77,7 +77,7 @@ void SqliteDataFrame::alloc_missing_cols() {
   }
 }
 
-void SqliteDataFrame::set_col_value(SEXP& col, const int j) {
+void SqliteDataFrame::set_col_value(RObject& col, const int j) {
   SEXPTYPE type = types[j];
   int column_type = sqlite3_column_type(stmt, j);
 

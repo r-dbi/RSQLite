e89214b54dd1d6521b42ecf102700e795ca557a9
Kirill Müller <krlmlr+r@mailbox.org>
2017-03-22 20:41:02 +0100

    use cstdint.hpp

--- src/sqlite3.h
+++ src/sqlite3.h
@@ -1,20 +1,15 @@
 #ifndef __RSQLITE_SQLITE_H
 #define __RSQLITE_SQLITE_H
 
-typedef struct _compound_int64_t {
-  uint32_t data[2];
-} compound_int64_t;
-
-// static assert
-typedef int sizeof_compound_int64_t_is_8[sizeof(compound_int64_t) == 8 ? 1 : -1];
+#include <boost/cstdint.hpp>
 
 // If you see "error: expected initializer before ‘sqlite_uint64’",
 // please patch sqlite3.h by running (from the root directory):
 //
 // patch -p1 < src-raw/sqlite3.patch
 
-#define SQLITE_INT64_TYPE compound_int64_t
-#define SQLITE_UINT64_TYPE compound_int64_t
+#define SQLITE_INT64_TYPE int64_t
+#define SQLITE_UINT64_TYPE uint64_t
 
 #include "sqlite3/sqlite3.h"
 

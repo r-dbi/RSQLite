7c8be91e22e44b665ad505ea2639daaedfc7a959
Kirill Müller <krlmlr+r@mailbox.org>
2017-11-30 11:48:28 +0100

    align types

--- src/RSQLite_types.h
+++ src/RSQLite_types.h
@@ -1,9 +1,21 @@
+#include "pch.h"
+
 #ifndef __RSQLSITE_TYPES__
 #define __RSQLSITE_TYPES__
 
 #include <RSQLite.h>
 
-#include "DbResult.h"
 #include "DbConnection.h"
+#include "DbResult.h"
 
-#endif // __RSQLSITE_TYPES__
+namespace Rcpp {
+
+template<>
+DbConnection* as(SEXP x);
+
+template<>
+DbResult* as(SEXP x);
+
+}
+
+#endif

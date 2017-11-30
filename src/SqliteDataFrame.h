446f7b1910d7696ef073e9f4fd157b5d1abf2127
Kirill Müller <krlmlr+r@mailbox.org>
2017-12-01 00:24:35 +0100

    extract superclass DbDataFrame

--- src/SqliteDataFrame.h
+++ src/SqliteDataFrame.h
@@ -1,36 +1,13 @@
 #ifndef RSQLITE_SQLITEDATAFRAME_H
 #define RSQLITE_SQLITEDATAFRAME_H
 
-
 #include "sqlite3.h"
-#include <boost/container/stable_vector.hpp>
-#include <boost/scoped_ptr.hpp>
-#include "DbColumnDataType.h"
+#include "DbDataFrame.h"
 
-class DbColumn;
-class DbColumnDataSourceFactory;
-
-class SqliteDataFrame {
-  boost::scoped_ptr<DbColumnDataSourceFactory> factory;
-  const int n_max;
-  int i;
-  boost::container::stable_vector<DbColumn> data;
-  std::vector<std::string> names;
-
+class SqliteDataFrame : public DbDataFrame {
 public:
   SqliteDataFrame(sqlite3_stmt* stmt, std::vector<std::string> names, const int n_max_, const std::vector<DATA_TYPE>& types);
-  ~SqliteDataFrame();
-
-public:
-  void set_col_values();
-  bool advance();
-
-  List get_data(std::vector<DATA_TYPE>& types);
-  size_t get_ncols() const;
-
-private:
-  void finalize_cols();
+  virtual ~SqliteDataFrame();
 };
-
 
 #endif //RSQLITE_SQLITEDATAFRAME_H

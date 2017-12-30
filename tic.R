6bcf671ead4d4f6bbca4b27bbafb784ee6e3e8a3
Kirill Müller <krlmlr+r@mailbox.org>
2017-12-30 20:32:29 +0100

    build docs for master

--- tic.R
+++ tic.R
@@ -9,10 +9,12 @@ get_stage("after_success") %>%
     )
   ))
 
-if (ci()$get_branch() == "production" && Sys.getenv("BUILD_PKGDOWN") != "") {
-  get_stage("deploy") %>%
+if (ci()$get_branch() == "master" && Sys.getenv("id_rsa") != "" && Sys.getenv("BUILD_PKGDOWN") != "") {
+  get_stage("before_deploy") %>%
     add_step(step_install_ssh_keys()) %>%
     add_step(step_test_ssh())
+
+  get_stage("deploy") %>%
     add_step(step_build_pkgdown()) %>%
     add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
 }

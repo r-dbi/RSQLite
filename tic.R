15e3638d7c89adfb1ecebabb966d0b1734b1c7a2
Kirill Müller <krlmlr+r@mailbox.org>
2018-02-02 08:42:39 +0100

    deploy to docs dir

--- tic.R
+++ tic.R
@@ -1,5 +1,4 @@
 get_stage("after_success") %>%
-  add_step(step_hello_world()) %>%
   add_step(step_run_code(
     covr::codecov(
       exclusions = c(
@@ -9,12 +8,14 @@ get_stage("after_success") %>%
     )
   ))
 
-if (ci()$get_branch() == "master" && Sys.getenv("id_rsa") != "" && Sys.getenv("BUILD_PKGDOWN") != "") {
+get_stage("deploy") %>%
+  add_step(step_build_pkgdown())
+
+if (Sys.getenv("id_rsa") != "" && Sys.getenv("BUILD_PKGDOWN") != "") {
   get_stage("before_deploy") %>%
     add_step(step_install_ssh_keys()) %>%
     add_step(step_test_ssh())
 
   get_stage("deploy") %>%
-    add_step(step_build_pkgdown()) %>%
-    add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
+    add_step(step_push_deploy())
 }

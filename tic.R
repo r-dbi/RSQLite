7478ef8f5879f626f6c53fcf30e88d9b56314788
Kirill Müller <krlmlr+r@mailbox.org>
2018-03-10 14:12:51 +0000

    Deploy from Travis build 1463 [ci skip]
    Build URL: https://travis-ci.org/r-dbi/RSQLite/builds/351701077
    Commit: 89e754ff2a600f5e1162fcdbfe88e0190f99037e

--- tic.R
+++ tic.R
@@ -8,14 +8,12 @@ get_stage("after_success") %>%
     )
   ))
 
-get_stage("deploy") %>%
-  add_step(step_build_pkgdown())
-
 if (Sys.getenv("id_rsa") != "" && Sys.getenv("BUILD_PKGDOWN") != "") {
   get_stage("before_deploy") %>%
     add_step(step_install_ssh_keys()) %>%
     add_step(step_test_ssh())
 
   get_stage("deploy") %>%
+    add_step(step_build_pkgdown()) %>%
     add_step(step_push_deploy())
 }

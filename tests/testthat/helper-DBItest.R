d3d94ebd743d8eda9b66fd3678cbef0244bc237f
Kirill Müller <krlmlr+r@mailbox.org>
2019-07-07 16:55:20 +0200

    - Use `default_skip`.

--- tests/testthat/helper-DBItest.R
+++ tests/testthat/helper-DBItest.R
@@ -12,5 +12,29 @@ DBItest::make_context(
     time_typed = FALSE,
     timestamp_typed = FALSE
   ),
-  name = "RSQLite"
+  name = "RSQLite",
+  default_skip = c(
+    # enable to test a particular test only
+    #"(?!data_timestamp_current).*",
+
+    # driver
+    "get_info_driver",                            # #117
+
+    # connection
+    "get_info_connection",                        # #117
+
+    # sql
+    "roundtrip_date",                             # #109
+    "roundtrip_timestamp",                        # #110
+
+    # meta
+    "get_info_result",                            # rstats-db/DBI#55
+
+    # transactions
+
+    # compliance
+    "compliance",                                 # skipping for now because of dbGetInfo()
+
+    NULL
+  )
 )
--- tests/testthat/test-DBItest.R
+++ tests/testthat/test-DBItest.R
@@ -1,28 +1,5 @@
 if (identical(Sys.getenv("NOT_CRAN"), "true")) {
 
-DBItest::test_all(c(
-  # enable to test a particular test only
-  #"(?!data_timestamp_current).*",
-
-  # driver
-  "get_info_driver",                            # #117
-
-  # connection
-  "get_info_connection",                        # #117
-
-  # sql
-  "roundtrip_date",                             # #109
-  "roundtrip_timestamp",                        # #110
-
-  # meta
-  "get_info_result",                            # rstats-db/DBI#55
-
-  # transactions
-
-  # compliance
-  "compliance",                                 # skipping for now because of dbGetInfo()
-
-  NULL
-))
+DBItest::test_all()
 
 }

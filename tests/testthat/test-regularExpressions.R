8eee0752df7929e7fc14c7fabc03906eae36c5a7
Ralf Herold <ralf.herold@gmx.net>
2019-07-10 21:04:50 +0200

    add tests for regular expressions

---
+++ tests/testthat/test-regularExpressions.R
@@ -0,0 +1,38 @@
+context("regexp")
+
+test_that("adding support for regular expressions(#296)", {
+
+  con <- dbConnect(
+    SQLite())
+
+  on.exit(
+    dbDisconnect(con),
+    add = TRUE)
+
+  initRegExp(
+    db = con)
+
+  dbWriteTable(
+    conn = con,
+    name = "iris",
+    value = iris)
+
+  # this regular expression is just to
+  # try out various syntax elements
+  res <- dbGetQuery(
+    conn = con,
+    statement = 'SELECT "Sepal.Length" FROM iris WHERE Species REGEXP "^v[ei]\\w*[^x-z\\\\s]+$";')
+
+  expect_equal(
+    object = nrow(res),
+    expected = 100L)
+
+  expect_true(
+    object = (6.2 < mean(res$Sepal.Length)) < 6.3)
+
+  res <- dbGetQuery(
+    conn = con,
+    statement = 'SELECT "Sepal.Length" FROM iris WHERE Species REGEXP "\\w";')
+
+})
+

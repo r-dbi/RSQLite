34d67b3abd91ccbb2ea76b9ea9b81835bfaddfad
Kirill Müller <krlmlr+r@mailbox.org>
2019-07-23 00:10:19 +0200

    Skip more tests for older DBItest

--- tests/testthat/test-DBItest.R
+++ tests/testthat/test-DBItest.R
@@ -3,7 +3,7 @@ if (identical(Sys.getenv("NOT_CRAN"), "true")) {
   if (packageVersion("DBItest") >= "1.6.0") {
     DBItest::test_all()
   } else {
-    DBItest::test_all(skip = default_skip)
+    DBItest::test_all(skip = c(default_skip, "roundtrip_raw", "roundtrip_blob"))
   }
 
 }

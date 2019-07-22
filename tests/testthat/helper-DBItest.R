4e33dfeb899fb403c73cff76c2b299b873cc6a7b
Kirill Müller <krlmlr+r@mailbox.org>
2019-07-22 12:09:28 +0200

    Conditional use of default_skip

--- tests/testthat/helper-DBItest.R
+++ tests/testthat/helper-DBItest.R
@@ -1,19 +1,5 @@
-DBItest::make_context(
-  SQLite(),
-  list(dbname = tempfile("DBItest", fileext = ".sqlite")),
-  tweaks = DBItest::tweaks(
-    constructor_relax_args = TRUE,
-    placeholder_pattern = c("?", "$1", "$name", ":name"),
-    date_cast = function(x) paste0("'", x, "'"),
-    time_cast = function(x) paste0("'", x, "'"),
-    timestamp_cast = function(x) paste0("'", x, "'"),
-    logical_return = function(x) as.integer(x),
-    date_typed = FALSE,
-    time_typed = FALSE,
-    timestamp_typed = FALSE
-  ),
-  name = "RSQLite",
-  default_skip = c(
+default_skip <-
+  c(
     # enable to test a particular test only
     #"(?!data_timestamp_current).*",
 
@@ -37,4 +23,40 @@ DBItest::make_context(
 
     NULL
   )
-)
+
+if (packageVersion("DBItest") >= "1.6.0") {
+  DBItest::make_context(
+    SQLite(),
+    list(dbname = tempfile("DBItest", fileext = ".sqlite")),
+    tweaks = DBItest::tweaks(
+      constructor_relax_args = TRUE,
+      placeholder_pattern = c("?", "$1", "$name", ":name"),
+      date_cast = function(x) paste0("'", x, "'"),
+      time_cast = function(x) paste0("'", x, "'"),
+      timestamp_cast = function(x) paste0("'", x, "'"),
+      logical_return = function(x) as.integer(x),
+      date_typed = FALSE,
+      time_typed = FALSE,
+      timestamp_typed = FALSE
+    ),
+    name = "RSQLite",
+    default_skip = default_skip
+  )
+} else {
+  DBItest::make_context(
+    SQLite(),
+    list(dbname = tempfile("DBItest", fileext = ".sqlite")),
+    tweaks = DBItest::tweaks(
+      constructor_relax_args = TRUE,
+      placeholder_pattern = c("?", "$1", "$name", ":name"),
+      date_cast = function(x) paste0("'", x, "'"),
+      time_cast = function(x) paste0("'", x, "'"),
+      timestamp_cast = function(x) paste0("'", x, "'"),
+      logical_return = function(x) as.integer(x),
+      date_typed = FALSE,
+      time_typed = FALSE,
+      timestamp_typed = FALSE
+    ),
+    name = "RSQLite"
+  )
+}
--- tests/testthat/test-DBItest.R
+++ tests/testthat/test-DBItest.R
@@ -1,5 +1,9 @@
 if (identical(Sys.getenv("NOT_CRAN"), "true")) {
 
-DBItest::test_all()
+  if (packageVersion("DBItest") >= "1.6.0") {
+    DBItest::test_all()
+  } else {
+    DBItest::test_all(skip = default_skip)
+  }
 
 }

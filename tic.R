41bca770f080265c2d3f80395d653b0d95e443d9
Kirill Müller <krlmlr+r@mailbox.org>
2019-12-14 23:50:56 +0100

    Also build on docs branch

--- tic.R
+++ tic.R
@@ -6,6 +6,6 @@ if (ci_has_env("DEV_VERSIONS")) {
 }
 
 # Build only for master or release branches
-if (ci_has_env("BUILD_PKGDOWN") && grepl("^master$|^r-", ci_get_branch())) {
+if (ci_has_env("BUILD_PKGDOWN") && grepl("^master$|^r-|^docs$", ci_get_branch())) {
   do_pkgdown()
 }

7cecb3249e26581d21974a8e5a499b28bd37fdf9
Kirill Müller <krlmlr+r@mailbox.org>
2019-12-18 02:44:50 +0100

    Upgrade RSQLite to 3.30.1

--- src/vendor/sqlite3/regexp.c
+++ src/vendor/sqlite3/regexp.c
@@ -58,7 +58,6 @@
 */
 #include <string.h>
 #include <stdlib.h>
-#include <R_ext/Visibility.h>
 #include "sqlite3ext.h"
 SQLITE_EXTENSION_INIT1
 
@@ -611,7 +610,7 @@ static const char *re_subcompile_string(ReCompiled *p){
 ** regular expression.  Applications should invoke this routine once
 ** for every call to re_compile() to avoid memory leaks.
 */
-void re_free(ReCompiled *pRe){
+static void re_free(ReCompiled *pRe){
   if( pRe ){
     sqlite3_free(pRe->aOp);
     sqlite3_free(pRe->aArg);
@@ -625,7 +624,7 @@ void re_free(ReCompiled *pRe){
 ** compiled regular expression in *ppRe.  Return NULL on success or an
 ** error message if something goes wrong.
 */
-const char *re_compile(ReCompiled **ppRe, const char *zIn, int noCase){
+static const char *re_compile(ReCompiled **ppRe, const char *zIn, int noCase){
   ReCompiled *pRe;
   const char *zErr;
   int i, j;
@@ -748,6 +747,7 @@ static void re_sql_func(
 #ifdef _WIN32
 __declspec(dllexport)
 #endif
+// Keep attribute_visible when upgrading
 attribute_visible int sqlite3_regexp_init(
   sqlite3 *db, 
   char **pzErrMsg, 

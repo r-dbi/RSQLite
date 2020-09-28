16a2cc8174852d3d62702342b6199148e7b97184
Kirill Müller <krlmlr+r@mailbox.org>
2020-09-28 11:11:28 +0200

    Tweak test

--- tests/testthat/test-blob.R
+++ tests/testthat/test-blob.R
@@ -7,6 +7,27 @@ test_that("adding large blob to table survives valgrind check (#192)", {
   con <- dbConnect(SQLite())
   on.exit(dbDisconnect(con), add = TRUE)
 
+  data <- data.frame(id = 1, data = I(list(raw(1e8))))
+  data$data <- unclass(data$data)
+  dbWriteTable(con, "data", data)
+
+  data$data <- blob::as_blob(data$data)
+  expect_equal(
+    dbReadTable(con, "data"),
+    data
+  )
+})
+
+test_that("can read more than standard limit (#314)", {
+  # Requires 64-bit system
+  skip_on_appveyor()
+
+  # Easy on CRAN's infrastructure
+  skip_on_cran()
+
+  con <- dbConnect(SQLite())
+  on.exit(dbDisconnect(con), add = TRUE)
+
   data <- data.frame(id = 1, data = blob(raw(1e9)))
   dbWriteTable(con, "data", data)
 

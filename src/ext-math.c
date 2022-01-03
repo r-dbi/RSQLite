3a2ad759a0bcd4b6a4946e783887e919a86274c9
Kirill Müller <krlmlr+r@mailbox.org>
2022-01-03 06:31:01 +0100

    Explain

--- src/ext-math.c
+++ src/ext-math.c
@@ -1,4 +1,6 @@
 #define SQLITE_CORE
 #define sqlite3_extension_init sqlite3_math_init
 #include <R_ext/Visibility.h>
+// File obtained from https://www.sqlite.org/contrib and tweaked
+// Not integrated into upgrade.R yet because upstream hasn't changed for a long time
 #include "vendor/sqlite3/extension-functions.c"

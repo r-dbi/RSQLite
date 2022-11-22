eb36c322c30872c1bfe683c7ddbc3b219d36e85b
Kirill Müller <krlmlr+r@mailbox.org>
2022-11-22 16:39:10 +0100

    Revert "Merge pull request #444 from r-dbi/f-sqlite-amalgamation-3400000"
    This reverts commit 40e56921616f03234ba362d0f51fcdf8a2f098ba, reversing
    changes made to 9c4ef9d5b359267404043c49205da02a1c6650b9.

--- src/vendor/extensions/csv.c
+++ src/vendor/extensions/csv.c
@@ -280,7 +280,6 @@ static char *csv_read_one_field(CsvReader *p){
     }
     p->cTerm = (char)c;
   }
-  assert( p->z==0 || p->n<p->nAlloc );
   if( p->z ) p->z[p->n] = 0;
   p->bNotFirst = 1;
   return p->z;
@@ -751,7 +750,7 @@ static int csvtabNext(sqlite3_vtab_cursor *cur){
       i++;
     }
   }while( pCur->rdr.cTerm==',' );
-  if( z==0 && i==0 ){
+  if( z==0 || (pCur->rdr.cTerm==EOF && i<pTab->nCol) ){
     pCur->iRowid = -1;
   }else{
     pCur->iRowid++;
@@ -812,12 +811,6 @@ static int csvtabFilter(
   CsvCursor *pCur = (CsvCursor*)pVtabCursor;
   CsvTable *pTab = (CsvTable*)pVtabCursor->pVtab;
   pCur->iRowid = 0;
-
-  /* Ensure the field buffer is always allocated. Otherwise, if the
-  ** first field is zero bytes in size, this may be mistaken for an OOM
-  ** error in csvtabNext(). */
-  if( csv_append(&pCur->rdr, 0) ) return SQLITE_NOMEM;
-
   if( pCur->rdr.in==0 ){
     assert( pCur->rdr.zIn==pTab->zData );
     assert( pTab->iStart>=0 );

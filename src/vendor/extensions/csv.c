454657a37a891731120dbaf26e9557c45a524643
Kirill Müller <krlmlr@users.noreply.github.com>
2025-05-03 17:36:20 +0200

    fix: Compilation with `gcc` works again (#594)

--- src/vendor/extensions/csv.c
+++ src/vendor/extensions/csv.c
@@ -39,7 +39,7 @@
 ** Some extra debugging features (used for testing virtual tables) are available
 ** if this module is compiled with -DSQLITE_TEST.
 */
-#include <sqlite3ext.h>
+#include "../extensions/sqlite3ext.h"
 SQLITE_EXTENSION_INIT1
 #include <string.h>
 #include <stdlib.h>

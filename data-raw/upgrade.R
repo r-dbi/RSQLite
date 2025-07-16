05534f0fa98aca173fdef563fc7c96f03e38ec4f
Kirill Müller <kirill@cynkra.com>
2025-07-16 10:46:10 +0200

    ci: Stabilize upgrade routine

--- data-raw/upgrade.R
+++ data-raw/upgrade.R
@@ -4,10 +4,12 @@ html <- xml2::read_html("https://www.sqlite.org/download.html")
 
 latest_name <- html %>%
   rvest::html_nodes("a") %>%
-  xml2::xml_find_all('//*[(@id = "a1")] | //*[(@id = "a2")] | //*[(@id = "a3")]') %>%
+  xml2::xml_find_all('//*[(@id = "a1")] | //*[(@id = "a2")] | //*[(@id = "a3")] | //*[(@id = "a4")]') %>%
   rvest::html_text() %>%
   grep("amalgamation", ., value = TRUE) %>%
   .[1]
+
+stopifnot(length(latest_name) == 1)
 
 tmp <- tempfile()
 

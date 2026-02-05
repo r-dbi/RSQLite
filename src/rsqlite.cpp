b63b7f111c20191c0a4b53aa931152d7d6ce188a
Kirill Müller <kirill@cynkra.com>
2026-02-05 08:34:17 +0100

    Prune

--- src/rsqlite.cpp
+++ src/rsqlite.cpp
@@ -9,8 +9,3 @@ cpp11::strings rsqliteVersion() {
   return cpp11::strings({ "header"_nm = SQLITE_VERSION,
                           "library"_nm = sqlite3_libversion() });
 }
-
-[[cpp11::register]]
-void init_logging(const std::string& log_level) {
-  plog::init_r(log_level);
-}

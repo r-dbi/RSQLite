dc861530749c6876aad7097f71b5cd15fc4968e5
davidrsch <daviddrsch@gmail.com>
2026-05-09 23:01:46 +0200

    Revert eager HTTP VFS registration from .onLoad()

--- R/zzz.R
+++ R/zzz.R
@@ -1,23 +1,5 @@
-# Package hooks
-
 .onLoad <- function(libname, pkgname) {
-  # Memoize one-time warnings early
-  if (!inherits(warning_once, "memoised")) {
-    warning_once <<- memoise::memoise(warning_once)
-  }
-
-  # Eagerly register HTTP VFS only if compiled in (compile-time gated)
-  if (exists("sqlite_httpvfs_compiled") && isTRUE(sqlite_httpvfs_compiled())) {
-    # Keep side effects minimal and ignore all errors; lazy path remains available
-    try(
-      {
-        con <- DBI::dbConnect(SQLite(), ":memory:")
-        on.exit(try(DBI::dbDisconnect(con), silent = TRUE), add = TRUE)
-        initExtension(con, "http")
-      },
-      silent = TRUE
-    )
-  }
+  warning_once <<- memoise::memoise(warning_once)
 }
 
 .onUnload <- function(libpath) {

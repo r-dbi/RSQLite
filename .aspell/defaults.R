1f3f7eebb2cb082f321a7222ca5a03f7f2c39ce0
Kirill Müller <kirill@cynkra.com>
2026-06-16 21:28:56 +0200

    aspell [ci skip]

--- .aspell/defaults.R
+++ .aspell/defaults.R
@@ -1,3 +1,5 @@
+# See tests/testthat/helper-aspell.R for the logic that creates the .rds file
+# (mentioned in the `dictionaries` argument) from the corresponding .txt file.
 Rd_files <- vignettes <- R_files <- description <-
     list(encoding = "UTF-8",
          language = "en",
---
+++ tests/testthat/helper-aspell.R
@@ -0,0 +1,21 @@
+local({
+  withr::local_dir("../..")
+  rds_files <- list.files(".aspell", pattern = "\\.rds$", full.names = TRUE)
+  stopifnot(length(rds_files) == 1)
+  txt_files <- list.files(".aspell", pattern = "\\.txt$", full.names = TRUE)
+  if (length(txt_files) == 0) {
+    rds <- readRDS(rds_files)
+    writeLines(rds, gsub("\\.rds$", ".txt", rds_files))
+    message("Created ", gsub("\\.rds$", ".txt", rds_files), " from ", rds_files)
+  } else {
+    stopifnot(length(txt_files) == 1)
+
+    info <- file.info(c(rds_files, txt_files))
+    txt_newer <- diff(info$mtime) > 0
+    if (txt_newer) {
+      txt <- readLines(txt_files)
+      saveRDS(txt, rds_files, version = 2)
+      message("Updated ", rds_files, " from ", txt_files)
+    }
+  }
+})
